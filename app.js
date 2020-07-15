32ac9e54746b0c3c9295203d827523041d15efdc
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-15 15:10:43 +1000

    connected to mangodb

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const RecipeSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    duration: {
+      type: String,
+      required: false
+    }
+})
+RecipeModel = mongoose.model("recipe", RecipeSchema)
+
+mongoose.connect(
+  "mongodb+srv://team-girls:ETsiWzgdCynyWMzZ@cluster0.zfqjd.mongodb.net/eco-recipe?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+  useUnifiedTopology: true },
+  error => {
+    if(error){
+      console.log("there was an error")
+      throw error
+    } else {
+      console.log("we are connected")
+    }
+  }
+)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  RecipeModel.create({
+      name: "cheese cake",
+      duration: "34"
+  })
+  .then(doc => res.status(200).send("<h1>works</h1>"))
+  .catch(err => res.status(400).send("<h1>Error</h1>"))
+  console.log("hello world2")
 });
 
 router.get('/test', function(req, res, next) {
