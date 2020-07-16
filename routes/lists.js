4f070363b7ff16c388dd2efc87ff04e6eaf76996
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-16 14:50:25 +1000

    all recipes name show in recipe page and added create recipe button to connect to back end

--- routes/lists.js
+++ routes/lists.js
@@ -5,15 +5,23 @@ const { route } = require('.')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  
   console.log(RecipeModel.find())
   RecipeModel.find()
   .then(doc => res.status(200).send(doc))
   .catch(err => res.status(400).send("<h1>Error</h1>"))
 });
 
-router.post('/', function(req, res) {
-  // console.log(req.body)
-  res.send(`you sent: ${JSON.stringify(req.body)}`)
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  const recipe = new RecipeModel(req.body)
+  try {
+    await recipe.save();
+    res.send(recipe);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 })
+
 
 module.exports = router;
