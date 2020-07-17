4d9c12d2efdf00204cd7f851603cb0ee299fd940
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-17 18:16:55 +1000

    added sign up page and sign up function

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose')
 const cors = require('cors')
+const passport = require('passport')
+const initializePassport = require('./passport-config')
+const flash = require('express-flash')
+const session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var listsRouter = require('./routes/lists');
@@ -26,12 +34,27 @@ mongoose.connect(
       }
     }
 )
-  
+
+const UserModel = require('./models/user')
+initializePassport(passport, e => {
+    UserModel.findOne({ email: e })
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}
+))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/lists', listsRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/user')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
 // const { route } = require('.')
 
 router.get('/', (req, res) =>
-    res.send('users home')
+    res.send(200)
 )
 
 router.get('/login', (req, res) =>
-    res.send('login')
+    res.send(200)
 )
 
-router.get('/register', (req, res) =>
-    res.send('hello')
+router.post('/login',
+
+(req, res) => console.log('testing')
 )
+
+router.get('/register', (req, res) => {
+    console.log('testing')
+    res.send('test')
+})
 
 router.post('/register', async (req, res) => {
-    const user = new UserModel(req.body)
     try {
-        await user.save();
-        res.send(user);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPassword)
+        const {name, email} = req.body
+        const user = new UserModel({name, email, password: hashedPassword})
+        user.save()
+        res.status(200).send()
     } catch (err) {
-        res.status(500).send(err);
+        res.status(404).send(err)
     }
 })
 
