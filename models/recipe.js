5979cd524a5dd3cff387c88ba9214ad2d045e1fb
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-24 21:49:14 +1000

    reverting

--- models/recipe.js
+++ models/recipe.js
@@ -1,50 +1,55 @@
 const mongoose = require('mongoose')
 
-const RecipesSchema = new mongoose.Schema({recipe: {
-    productName: {
-      type: String,
+// const RecipesSchema = new mongoose.Schema({recipe: {
+const RecipesSchema = new mongoose.Schema({
+  publicId: {
+    type: String,
+    required: true
+  },
+  productName: {
+    type: String,
+    required: true
+  },
+  productSummary: {
+    type: String
+  },
+  prepTime: {
+    type: Number,
+    required: false,
+    // validate(value) {
+    //   if (value < 0) {
+    //     throw new Error("Prep Time must be greater than 0")
+    //   }
+    // }
+  },
+  ingredients: [
+    {
+      ingredientName: { type: [String], required: true },
+      amount: {
+        ingredientAmount: [String],
+        unitName: String
+      },
+      unit: {
+        amountUnit: String,
+        enum: ['tsp', 'tbl', 'cup(s)', 'drop(s)', 'ml', 'L', 'g', 'unit'],
+      }
+    }],
+  instructions: [
+    {
+      type: [String],
       required: true
-    },
-    productSummary:{
-      type: String
-    },
-    prepTime: {
-      type: Number,
-      required: false,
-      // validate(value) {
-      //   if (value < 0) {
-      //     throw new Error("Prep Time must be greater than 0")
-      //   }
-      // }
-    },
-    ingredients: [
-      {
-        ingredientName: {type: [String], required: true},
-        amount: {
-          ingredientAmount: [String],
-          unitName: String
-        },
-        unit: {
-          amountUnit: String,  
-          enum: ['tsp', 'tbl', 'cup(s)', 'drop(s)', 'ml', 'L', 'g', 'unit'], 
-        }
-     }],
-    instructions: [
-        {
-          type: [String],
-          required: true
-        }
-    ],
-    category: {
-      type: String,
-      // enum: ['Skincare', 'Home Care', 'Personal Care', 'Cleaning'],
-      required: true
-    },
-    tags: [
-        {type: String}
-    ]
-}})
+    }
+  ],
+  category: {
+    type: String,
+    enum: ['Skincare', 'Home Care', 'Personal Care', 'Cleaning'],
+    required: true
+  },
+  tags: [
+    { type: String }
+  ]
+})
 
 
- 
+
 module.exports = mongoose.model("recipe", RecipesSchema)
