c5203e2cbbe5a3ad06a2dcc093782c37ea520f28
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-28 11:27:39 +1000

    changed the authlize file and update router

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,8 +1,8 @@
 module.exports = {
     isLoggedIn: function(req,res,next) {
-   if(req.userId) {
+   if(req.body.recipe.userId) {
    return next()
-   } else if(!req.userId){
+   } else if(!req.body.recipe.userId){
     res.status(404).send({message: "Please log in to continue"})
    } else {
     res.status(404).send({message: "Something went wrong"})
