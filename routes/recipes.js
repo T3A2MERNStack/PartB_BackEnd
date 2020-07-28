cab0cce007bd7c1429c99ed18bb5cfc4e4885693
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-28 17:49:22 +1000

    added recipe tags

--- routes/recipes.js
+++ routes/recipes.js
@@ -18,6 +18,13 @@ router.get('/category/:id', async (req, res) =>{
   res.send(recipes)
 })
 
+router.get('/categoryc/:id', async (req, res) =>{
+  // console.log(req.params.id)
+  const recipes = await RecipeModel.find({category: "cleaning"})
+  // console.log(recipes)
+  res.send(recipes)
+})
+
 /* GET users listing. */
 router.get('/lists', function(req, res, next) {
   // console.log(RecipeModel.find({"test": "test"}))
